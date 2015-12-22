b034bd3bddbac7d7adecc2fae527a3cc431a2f75
zanjs <root@zanjs.com>
2015-12-22 11:53:18 +0800

    updated judoc.config.js

--- judoc.config.js
+++ judoc.config.js
@@ -4,7 +4,7 @@ module.exports = {
   name: 'Jujs',
   version: pkg.version,
   repositoryUrl: pkg.homepage,
-  introduction: 'Jujs，一个javascript常用方法库，包含了浏览器中对storage、cookie的操作封装，e5 shim，date日期处理，身份证、url中元素校验解析，及一些其他常用的方法集合（正则、extend、继承、类等...）。<br /><br />源码采用AMD书写，以webpack打包，karma+jasmine单元测试，UMD格式输出，如果以全局方式引入，方法集全挂在全局变量Ju下，例如访问util方法集则已Ju.util访问。<br /><br /><a href="/fe/static/scripts/Ju.min.js" target="_blank">生产版本（' + pkg.version + '）</a><br /><a href=\"/fe/static/scripts/Ju.js\" target="_blank">开发版本（' + pkg.version + '）</a>',
+  introduction: 'Jujs，javascript常用方法库,不依赖任何第三库，包含了浏览器中对storage、cookie的操作封装，e5 shim，date日期处理，身份证、url中元素校验解析，及一些其他常用的方法集合（正则、extend、继承、类等...）。<br /><br />源码采用AMD书写，以webpack打包，karma+jasmine单元测试，UMD格式输出，如果以全局方式引入，方法集全挂在全局变量Ju下，例如访问util方法集则已Ju.util访问。<br /><br /><a href="../build/Ju.min.js" target="_blank">生产版本（' + pkg.version + '）</a><br />',
   paths: {
     input: "./src",
     output: "./doc"
